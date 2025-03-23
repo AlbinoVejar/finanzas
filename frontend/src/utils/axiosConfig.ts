94a456554a6a660a690b7e852db2254112b4843d
AlbinoVejar <57921900+AlbinoVejar@users.noreply.github.com>
2025-03-23 20:28:06 +0000

    logs

--- frontend/src/utils/axiosConfig.ts
+++ frontend/src/utils/axiosConfig.ts
@@ -4,6 +4,7 @@ import { setupInterceptorsTo } from "../middlewares/requets";
 
 const apiURL = import.meta.env.VITE_APP_ENV;
 const prodURL = import.meta.env.VITE_APP_APIURL;
+console.log("ENV: ", apiURL, prodURL);
 
 const axiosConfig = setupInterceptorsTo(axios.create({
   baseURL: apiURL === "production" ? prodURL : url_localhost,
