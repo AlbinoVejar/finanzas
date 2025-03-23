cb0ff3e3673512675d9c3f9b8bd0f8dd345186a4
AlbinoVejar <57921900+AlbinoVejar@users.noreply.github.com>
2025-03-23 00:07:49 +0000

    secret key in env

--- backend/src/config/config.go
+++ backend/src/config/config.go
@@ -1,5 +1,8 @@
 package config
 
-const SECRET_KEY = "njxiGw7O4SqLUu//j6WB62jt0Pu+AEkgdQczTixrTeO5HWmaNkIIEppAWk3rlAps0aU8G/mcjMuXPdIv"
+import "os"
+
+var SECRET_KEY = os.Getenv("SECRET_KEY")
+
 const db_connection_localhost = "%s:%s@tcp(%s:%s)/%s"
 const db_connection_prod = "%s:%s@tcp(%s:%s)/%s"
