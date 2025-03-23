a925a1536c939d4c8651c1c761f87d05accd14e1
AlbinoVejar <57921900+AlbinoVejar@users.noreply.github.com>
2025-03-23 21:30:07 +0000

    fix service

--- frontend/src/services/auth.service.ts
+++ frontend/src/services/auth.service.ts
@@ -1,13 +1,12 @@
-import axios from "axios"
-import { url_localhost } from "../shared/enviroment"
 import { ResponseAPI } from "../types/response.type"
 import { LoginType } from "../types/auth.type"
+import axiosConfig from "../utils/axiosConfig";
 
 const mainUrl: string = '/users'
 
 export const Login = async (form: LoginType): Promise<ResponseAPI<string>> => {
   try {
-    const { data } = await axios.post<ResponseAPI<string>>(`${url_localhost}${mainUrl}/login`, form)
+    const { data } = await axiosConfig.post<ResponseAPI<string>>(`${mainUrl}/login`, form)
     return data
   } catch (error: any) {
     throw error;
