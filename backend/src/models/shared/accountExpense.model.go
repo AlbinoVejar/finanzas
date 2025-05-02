b1cfe6dbdf46b304ed66f7e0a9a13cbd44d43f19
Albino Vejar <albinovejar@hotmail.com>
2025-05-01 18:58:12 -0700

    new controller function for new endpoint

--- backend/src/models/shared/accountExpense.model.go
+++ backend/src/models/shared/accountExpense.model.go
@@ -3,7 +3,7 @@ package shared
 import "gitlab.com/AlbinoVejar/finanzas/backend/src/models"
 
 type AccountExpensesResponse struct {
-	Accounts []models.AccountDashboard
+	Accounts []models.AccountTotalResponse
 	Expenses []models.ExpenseByAccount
 	Total    float32
 }
