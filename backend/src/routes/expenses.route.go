8f96019b012365267117845d41f8a9f631de60e4
Albino Vejar <albinovejar@hotmail.com>
2025-05-01 18:58:28 -0700

    everything for the new endpoint

--- backend/src/routes/expenses.route.go
+++ backend/src/routes/expenses.route.go
@@ -12,5 +12,6 @@ func RoutesExpenses(app *fiber.App) {
 	route.Post("/", middlewares.Protected(), controllers.CreateExpense)
 	route.Put("/:id", middlewares.Protected(), controllers.UpdateExpense)
 	route.Get("/totals", middlewares.Protected(), controllers.GetExpensesByAccount)
+	route.Get("/dashboard", middlewares.Protected(), controllers.GetDashboardByUser)
 	route.Delete("/:id", middlewares.Protected(), controllers.DeleteExpense)
 }
